6085443652e62e4fb5e7d20b7b4dd53fc592544b
Askhabov-HM <askhamzat@mail.ru>
2021-04-06 17:23:01 +0300

    save progress

--- script/script.js
+++ script/script.js
@@ -5,4 +5,11 @@ $(document).ready( ()=>{
     $('.call_form-phone').mask('+7(000)000-00-00');
     $('.modal_form-phone').mask('+7(000)000-00-00')
     //JQUERY MASK END
+
+
+    // MODAL
+    $('#modal').on('hidden.bs.modal', function(){
+        // setTimeout(alert(123), 2000)
+    })
+
 })
